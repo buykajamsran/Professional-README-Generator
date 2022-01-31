f7cf55a99096d7b8db364bd2e31b0036ad385404
buykajamsran <buyka.jamsran@gmail.com>
2022-01-30 18:44:51 -0600

    Function to initialize app created and updated questions for user input

--- Develop/index.js
+++ Develop/index.js
@@ -4,112 +4,115 @@ const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const promptUser = () => {
-    return inquirer.prompt([
-        {
-            type: 'input',
-            name: 'title',
-            message: 'Enter project title (required)',
-            validate: titleInput => {
-                if (titleInput) {
-                    return true;
-                } else {
-                    console.log('Project title required');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'description',
-            message: 'Enter a short description explaining the what, why, and how of your project (required)',
-            validate: descriptionInput => {
-                if (descriptionInput) {
-                    return true;
-                } else {
-                    console.log('Project description required');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'install',
-            message: 'Enter the steps required to install your project (required)',
-            validate: installInput => {
-                if (installInput) {
-                    return true;
-                } else {
-                    console.log('Installation steps required');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'usage',
-            message: 'Enter instructions and examples for use (required)',
-            validate: usageInput => {
-                if (usageInput) {
-                    return true;
-                } else {
-                    console.log('Project instructions required');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'contribute',
-            message: 'List your collaborators, if any, with links to their GitHub profiles',
-        },
-        {
-            type: 'input',
-            name: 'test',
-            message: 'Write tests for your application',
-        },
-        {
-            type: 'list',
-            name: 'license',
-            message: 'Choose a licence for your project',
-            choices: [],
-        },
-        {
-            type: 'input',
-            name: 'github',
-            message: 'Enter your GitHub username (required)',
-            validate: githubInput => {
-                if (githubInput) {
-                    return true;
-                } else {
-                    console.log('GitHub username required');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: 'Enter your email address (required)',
-            validate: emailInput => {
-                if (emailInput) {
-                    return true;
-                } else {
-                    console.log('Email address required');
-                    return false;
-                }
+const promptUser = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Enter project title (required)',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Project title required');
+                return false;
             }
         }
-    ]);
-};
-
-promptUser().then(answers => console.log(answers))
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Enter a short description explaining the what, why, and how of your project (required)',
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log('Project description required');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: 'Enter the steps required to install your project (required)',
+        validate: installInput => {
+            if (installInput) {
+                return true;
+            } else {
+                console.log('Installation steps required');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Enter instructions and examples for use (required)',
+        validate: usageInput => {
+            if (usageInput) {
+                return true;
+            } else {
+                console.log('Project instructions required');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'contribute',
+        message: 'List your collaborators, if any, with links to their GitHub profiles',
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Write tests for your application',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a licence for your project',
+        choices: ['MIT', 'none'],
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub username (required)',
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log('GitHub username required');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address (required)',
+        validate: emailInput => {
+            if (emailInput) {
+                return true;
+            } else {
+                console.log('Email address required');
+                return false;
+            }
+        }
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const writeToFile = (fileName, data) => {
+    fs.writeFile(`./dist/${fileName}`, generateMarkdown(data), err => err && console.log(err));
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+const init = () => {
+    return inquirer.prompt(promptUser)
+    .then(readmeData => {
+        console.log(readmeData);
+    });
+};
 
 // Function call to initialize app
 init();
